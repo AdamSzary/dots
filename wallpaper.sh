d90e498ebc09caa1148ccfb4b5525471fa665a6f
Szary <szary@tenteg.us>
2026-01-29 10:22:42 +0100

    Added the Wallpaper Engine script I use and my fuzzel config

---
+++ wallpaper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Using the https://github.com/Almamu/linux-wallpaperengine project
+
+linux-wallpaperengine \
+   --screen-root DP-2 --bg 2903579030 \
+   --screen-root DP-1 --bg 2903579030 \
+   --fps 30
